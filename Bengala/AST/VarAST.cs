5de7a54eb85d58d3ecf3c1f3da4a9b901c862ab5
Raimil Cruz <raimil.cruz@gmail.com>
2019-08-12 12:17:30 -0400

    Remove property Semantic Model from VarAST
    It does not correspond to this change.

--- Bengala/AST/VarAST.cs
+++ Bengala/AST/VarAST.cs
@@ -1,14 +1,4 @@
-﻿#region Usings
-
-using System;
-using System.Collections.Generic;
-using System.Reflection.Emit;
-using Bengala.AST.SemanticModel;
-using Bengala.AST.SemanticsUtils;
-
-#endregion
-
-namespace Bengala.AST
+﻿namespace Bengala.AST
 {
     /// <summary>
     /// Representa una variable : "var" + 3 .Tambien puede ser usado para representa la asignacion a una variable
@@ -17,7 +7,7 @@ namespace Bengala.AST
     {
         #region Fields and Properties
 
-        public string VarId { get; private set; }
+        public string VarId { get; }
 
         #endregion
 
@@ -33,8 +23,6 @@ namespace Bengala.AST
 
       
         public bool IsForeignVar { get; set; }
-
-        public SemanticElement SemanticElement { get; set; }
 
         public override T Accept<T>(AstVisitor<T> visitor)
         {
